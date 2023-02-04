91f5e065ecc1161a6c08388c21335ee436d21bab
Aewenor <bartix12-1995@o2.pl>
2023-02-04 12:26:03 +0100

    Impement of Reader thread

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
-int main() {
-   return 0;
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_t thread_ids[5]; //IDs of threads
+int cpuValues[10]; //Storage for cpu values
+
+void *Reader(void *thid){
+
+   FILE* fp = fopen("/proc/stat","r"); //open file
+
+   fscanf(fp, "%*s %d %d %d %d %d %d %d %d %d %d", &cpuValues[0], &cpuValues[1],&cpuValues[2],&cpuValues[3],&cpuValues[4],&cpuValues[5],&cpuValues[6],&cpuValues[7],&cpuValues[8],&cpuValues[9]);
+
+   fclose(fp); //close file
+
+   printf("User: %d Nice: %d System: %d Idle: %d iowait: %d irq: %d softirq: %d Steal: %d Guest: %d Guest_nice: %d", cpuValues[0], cpuValues[1],cpuValues[2],cpuValues[3],cpuValues[4],cpuValues[5],cpuValues[6],cpuValues[7],cpuValues[8],cpuValues[9]); //print values (temporary help)
+
+   sleep(1); //wait a second
+}
+
+int main(){
+   pthread_create(&thread_ids[0], NULL, Reader, NULL);
+   pthread_join(thread_ids[0], NULL);
 }
